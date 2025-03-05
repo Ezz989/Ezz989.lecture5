959756de418802a28ff48ab0dd673c33135da121
Ezz989 <ershad0025@gmail.com>
2025-03-05 09:37:01 +0100

    Update 1

---
+++ lecture5.js
@@ -0,0 +1,7 @@
+document.getElementById("courseButton").onclick = function() {
+    window.location.href = 'courses.html';
+};
+
+document.getElementById("backButton").onclick = function() {
+    window.location.href = 'lecture5.html';
+};  
